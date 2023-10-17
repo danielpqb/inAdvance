451baea74f59fd231d198722673076d6c12eda63
danielpqb <danielpqb@gmail.com>
2023-10-17 17:22:44 -0300

    feat: LoanCard

---
+++ src/components/LoanCard.tsx
@@ -0,0 +1,95 @@
+import { gSC, gStyles } from "@/styles/global";
+import { FC } from "react";
+import { StyleSheet, View, Text } from "react-native";
+import Button from "./Button";
+import { useAppContext } from "@/contexts/AppContext";
+import { monetaryNumberToString } from "@/utils/monetary-value-converter";
+
+const styles = StyleSheet.create({
+  view: {
+    ...gStyles.growCenter,
+    backgroundColor: gSC("white", 0.1),
+    padding: 10,
+    flex: 0,
+    borderRadius: 8,
+    borderWidth: 2,
+    borderColor: gSC("zinc300", 0.2),
+    gap: 5,
+    width: "100%",
+  },
+  button: {
+    backgroundColor: undefined,
+    padding: undefined,
+    borderWidth: 0,
+    width: "100%",
+  },
+  infoView: {
+    width: "100%",
+  },
+  label: { fontSize: 12, fontWeight: "600", color: gSC("zinc100", 0.5) },
+  text: { fontSize: 14, fontWeight: "500", color: gSC("zinc100") },
+  bottomView: {
+    width: "100%",
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  bottomInfoView: { marginTop: 5 },
+  bottomInfoLabel: {
+    fontSize: 14,
+    fontWeight: "600",
+    color: gSC("zinc100", 0.5),
+  },
+  bottomInfoText: { fontSize: 24, fontWeight: "700", color: gSC("yellow300") },
+});
+
+type TLoanCardProps = {
+  data: {
+    name: string;
+    total: number;
+    description: string;
+  };
+};
+const LoanCard: FC<TLoanCardProps> = ({ data }) => {
+  const { changeNavigationMode } = useAppContext();
+
+  return (
+    <Button
+      style={{ ...styles.button }}
+      onLongPress={() => {
+        changeNavigationMode("delete");
+      }}
+      onPress={async () => {
+        console.warn("Navegar para Parcelas desse empréstimo!");
+      }}
+    >
+      <View style={{ ...styles.view }}>
+        <View style={{ ...styles.infoView }}>
+          <Text style={{ ...styles.label }}>Cliente</Text>
+          <Text style={{ ...styles.text }}>{data.name}</Text>
+        </View>
+        <View style={{ ...styles.infoView }}>
+          <Text style={{ ...styles.label }}>Descrição</Text>
+          <Text style={{ ...styles.text }}>{data.description}</Text>
+        </View>
+        <View style={{ ...styles.bottomView }}>
+          <View style={{ ...styles.bottomInfoView }}>
+            <Text style={{ ...styles.bottomInfoLabel }}>Restante a Pagar</Text>
+            <Text style={{ ...styles.bottomInfoText }}>
+              {monetaryNumberToString(123456)}
+            </Text>
+          </View>
+          <View style={{ ...styles.bottomInfoView }}>
+            <Text style={{ ...styles.bottomInfoLabel, textAlign: "right" }}>
+              Parcelas Pagas
+            </Text>
+            <Text style={{ ...styles.bottomInfoText, textAlign: "right" }}>
+              10/12
+            </Text>
+          </View>
+        </View>
+      </View>
+    </Button>
+  );
+};
+
+export default LoanCard;
--- src/screens/LoansScreen.tsx
+++ src/screens/LoansScreen.tsx
@@ -1,21 +1,78 @@
+import LoanCard from "@/components/LoanCard";
+import ScrollContainer from "@/components/ScrollContainer";
 import { gSC, gStyles } from "@/styles/global";
 import { FC } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, View } from "react-native";
 
 const styles = StyleSheet.create({
-  view: { ...gStyles.growCenter },
+  view: {
+    ...gStyles.growCenter,
+    gap: 10,
+    padding: 15,
+    justifyContent: "flex-start",
+  },
   text: {
-    color: gSC("orange600"),
-    fontSize: 36,
+    color: gSC("zinc200"),
+    fontSize: 50,
+    paddingBottom: 20,
   },
 });
+
+const loansData = [
+  {
+    name: "Isabel Pereira Queiroz Barros",
+    total: 156000,
+    description:
+      "Sapato preto de muitas funcionalidades que deve ser usado diariamente para conforto dos pés",
+  },
+  {
+    name: "Daniel Pereira Queiroz Barros",
+    total: 23410,
+    description:
+      "Sapato preto de muitas funcionalidades que deve ser usado diariamente para conforto dos pés",
+  },
+  {
+    name: "Vanessa Gomes Queiroz Barros",
+    total: 52000,
+    description:
+      "Sapato preto de muitas funcionalidades que deve ser usado diariamente para conforto dos pés",
+  },
+  {
+    name: "Julia Pereira Queiroz Barros",
+    total: 2159070,
+    description:
+      "Sapato preto de muitas funcionalidades que deve ser usado diariamente para conforto dos pés",
+  },
+  {
+    name: "Leonardo Queiroz Barros",
+    total: 716000,
+    description:
+      "Sapato preto de muitas funcionalidades que deve ser usado diariamente para conforto dos pés",
+  },
+  {
+    name: "Marília Gomes",
+    total: 1061030,
+    description:
+      "Sapato preto de muitas funcionalidades que deve ser usado diariamente para conforto dos pés",
+  },
+  {
+    name: "Juliane Carneiro",
+    total: 122002,
+    description:
+      "Sapato preto de muitas funcionalidades que deve ser usado diariamente para conforto dos pés",
+  },
+];
 
 type TLoansScreenProps = {};
 const LoansScreen: FC<TLoansScreenProps> = () => {
   return (
-    <View style={{ ...styles.view }}>
-      <Text style={{ ...styles.text }}>Loans!</Text>
-    </View>
+    <ScrollContainer style={{ paddingBottom: 70, paddingTop: 0 }}>
+      <View style={{ ...styles.view }}>
+        {loansData.map((data) => {
+          return <LoanCard data={data} />;
+        })}
+      </View>
+    </ScrollContainer>
   );
 };
 
